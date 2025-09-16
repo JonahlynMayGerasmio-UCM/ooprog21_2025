2ea3115bbb473d1e073ae26bd1feee17c0da7bc6
JonahlynMayGerasmio-UCM <jonahmgerasmio@gmail.com>
2025-09-16 08:50:25 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Will you still love me");
+        System.out.println("When I'm no longer young and beautiful?");
+        System.out.println("Will you still love me");
+        System.out.println("When I got nothing but my aching soul?");
+    }
+}
