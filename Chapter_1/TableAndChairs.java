4d159d7828f888420cb9f0a9f7f19c866246c90a
JonahlynMayGerasmio-UCM <jonahmgerasmio@gmail.com>
2025-09-17 22:06:06 +0800

    Add files via upload

--- Chapter_1/TableAndChairs.java
+++ Chapter_1/TableAndChairs.java
@@ -1,10 +1,20 @@
 public class TableAndChairs {
     public static void main(String[] args) {
-        System.out.println("X                      X");
-        System.out.println("X                      X");
+
+        // First 2 rows: "X                      X"
+        for (int i = 0; i < 2; i++) {
+            System.out.println("X                      X");
+        }
+
+        // Third row: "X      XXXXXXXXXX      X"
         System.out.println("X      XXXXXXXXXX      X");
+
+        // Fourth row: "XXXXX  X        X  XXXXX"
         System.out.println("XXXXX  X        X  XXXXX");
-        System.out.println("X   X  X        X  X   X");
-        System.out.println("X   X  X        X  X   X");
+
+        // Last 2 rows: "X   X  X        X  X   X"
+        for (int i = 0; i < 2; i++) {
+            System.out.println("X   X  X        X  X   X");
+        }
     }
 }
